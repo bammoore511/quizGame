14839b4c93353dbec76e4463210aff293dc5bc47
Brody Moore <bammoore511@gmail.com>
2021-09-28 15:40:46 -0400

    implemented choosing question difficulty

--- main.py
+++ main.py
@@ -2,33 +2,24 @@ from question_model import Question
 from quiz_brain import QuizBrain
 import requests
 # Question data from Open Trivia DB API
-trivia = requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean")
-
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+}
+difficulty = input("Difficulty (Easy/Medium/Hard): ").lower()
+parameters["difficulty"] = difficulty
+trivia = requests.get(url="https://opentdb.com/api.php", params=parameters)
+trivia.raise_for_status()
 question_data = trivia.json()['results']
 
 # create banks of questions
-easy_questions = []
-medium_questions = []
-hard_questions = []
+questions = []
+
 # fill banks with data from Open Quiz DB
-for question in easy_data:
+for question in question_data:
     question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
-    easy_questions.append(new_question)
+    questions.append(new_question)
 
-for question in medium_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
-    medium_questions.append(new_question)
-
-for question in hard_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
-    hard_questions.append(new_question)
-
-easy_quiz = QuizBrain(easy_questions)
-medium_quiz = QuizBrain(medium_questions)
-hard_quiz = QuizBrain(hard_questions)
+quiz = QuizBrain(questions)
